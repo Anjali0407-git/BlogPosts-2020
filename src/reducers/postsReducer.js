db79c8e1f1fcc70072393ac939abaa2337ef66c2
unknown <you@example.com>
2021-01-12 00:00:37 -0800

    using redux

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,13 +1,5 @@
-import axios from "axios";
-// export const PostsReducer = (state, action) => {
-   
-//     // async function loadPosts(){
-//     //     const result = await axios.get("http://localhost:5000/posts");
-//     //     return result.data[0]
-//     //   }
-    var allPosts= [{title: "anjali",body: "anjali"},{title:"jaanu",body:"jaanu"}]
-    let defaultState= {
-        dummyPosts:[{title:"radha",body:"radha"}],
+let defaultState= {
+        dummyPosts:[],
         viewPost :{title:'' , body: ''}
     }
 export const postsReducer=(state=defaultState,action)=>{
@@ -27,23 +19,8 @@ export const postsReducer=(state=defaultState,action)=>{
                 dummyPosts : action.payload
             }
         default:
-            console.log("in reducers, default")
             console.log(state)
             return state;
     }
 }
 
-// const postsReducer=(state=defaultState,action)=>{
-//     if(action.type==="SEND_POSTS"){
-//         return{
-//             ...state,
-//             dummyPosts:action.payload
-//         }
-//     }
-//     else{
-//         return {
-//             ...state
-//         }
-//     }
-// }
-// export default postsReducer;
